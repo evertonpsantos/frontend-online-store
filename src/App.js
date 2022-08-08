90bcede47929d8e3261431b8484f5561f8389f43
Gustavo <vasconcelosgu604@gmail.com>
2022-08-07 21:49:43 -0300

    :sparkles: Requisito 7

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Search from './pages/Search';
 import Cart from './pages/Cart';
+import Item from './pages/Item';
 
 class App extends React.Component {
   render() {
@@ -9,6 +10,7 @@ class App extends React.Component {
       <BrowserRouter>
         <Route exact path="/" component={ Search } />
         <Route exact path="/cart" component={ Cart } />
+        <Route exact path="/item/:id" component={ Item } />
       </BrowserRouter>
     );
   }
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 export default class ProductCard extends Component {
   render() {
     const { info } = this.props;
-    const { price, title, thumbnail } = info;
+    const { price, title, thumbnail, id } = info;
     return (
       <main>
         { info && (
+
           <div data-testid="product">
             <h4>
               { title }
@@ -17,6 +19,12 @@ export default class ProductCard extends Component {
               src={ thumbnail }
               alt={ title }
             />
+            <Link
+              data-testid="product-detail-link"
+              to={ `/item/${id}` }
+            >
+              Detalhes:
+            </Link>
           </div>
         )}
       </main>
